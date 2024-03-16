c52cf9ee906d1d06a303d7b4208224623a216a5e
Ireme-Nyawe <akimanaj41@gmail.com>
2024-03-16 09:39:22 +0200

    design signup form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,3 +1,16 @@
-export default function Form(){
-    return ("Horm Here")
+export default function Form() {
+  return (
+    <form className="form">
+      <input type="text"  className="form-item" placeholder="Email"/>
+      <input type="text"  className="form-item" placeholder="Password"/>
+      <input type="text"  className="form-item" placeholder="Confirm Password"/>
+      <div className="form-item">
+        <input type="checkbox" />
+        <label htmlFor="">I want to join newsletter</label>
+      </div>
+      <div className="form-item div-button">
+        <button>Sign Up</button>
+      </div>
+    </form>
+  );
 }
